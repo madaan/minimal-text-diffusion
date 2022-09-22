f73c90df45752717b1e4eba2c110a4c44b126840
Aman Madaan <amadaan@cs.cmu.edu>
2022-09-22 14:06:05 -0400

    [mini] remove loss functions that are not used

--- gaussian_diffusion.py
+++ gaussian_diffusion.py
@@ -12,7 +12,7 @@ import numpy as np
 import torch as th
 
 from nn import mean_flat
-from losses import normal_kl, discretized_gaussian_log_likelihood, discretized_text_log_likelihood
+from losses import normal_kl, discretized_gaussian_log_likelihood
 
 
 def get_named_beta_schedule(schedule_name, num_diffusion_timesteps):
@@ -236,6 +236,7 @@ class GaussianDiffusion:
         #     self.training_losses = self.training_losses_emb
 
     def training_losses(self, model, *args, **kwargs):
+        return self.training_losses_actual(model, *args, **kwargs)
         if self.training_mode == 'e2e':
             return self.training_losses_e2e(model, *args, **kwargs)
         elif self.training_mode == 'e2e-simple':
@@ -1169,110 +1170,6 @@ class GaussianDiffusion:
         output = kl + decoder_nll + kl_T 
         return {"output": output, "pred_xstart": out["pred_xstart"], 'kl': kl, 'decoder_nll':decoder_nll, 'kl_T':kl_T}
 
-    def training_losses_emb(self, model, x_start, t, model_kwargs=None, noise=None):
-        """
-        Compute training losses for a single timestep.
-
-        :param model: the model to evaluate loss on.
-        :param x_start: the [N x C x ...] tensor of inputs.
-        :param t: a batch of timestep indices.
-        :param model_kwargs: if not None, a dict of extra keyword arguments to
-            pass to the model. This can be used for conditioning.
-        :param noise: if specified, the specific Gaussian noise to try to remove.
-        :return: a dict with the key "loss" containing a tensor of shape [N].
-                 Some mean or variance settings may also have other keys.
-        """
-        if model_kwargs is None:
-            model_kwargs = {}
-        if noise is None:
-            noise = th.randn_like(x_start)
-        x_t = self.q_sample(x_start, t, noise=noise)
-
-        terms = {}
-
-        if self.loss_type == LossType.KL or self.loss_type == LossType.RESCALED_KL:
-            terms["loss"] = self._vb_terms_bpd(
-                model=model,
-                x_start=x_start,
-                x_t=x_t,
-                t=t,
-                clip_denoised=False,
-                model_kwargs=model_kwargs,
-                noise=noise,
-            )["output"]
-            if self.loss_type == LossType.RESCALED_KL:
-                # print(terms["loss"])
-                terms["loss"] *= self.num_timesteps
-        elif self.loss_type == LossType.MSE or self.loss_type == LossType.RESCALED_MSE:
-            if model_kwargs is not None and 'input_ids' in model_kwargs:
-                model_kwargs.pop('input_ids')
-            model_output = model(x_t, self._scale_timesteps(t), **model_kwargs)
-
-            if self.model_var_type in [
-                ModelVarType.LEARNED,
-                ModelVarType.LEARNED_RANGE,
-            ]:
-                if self.model_arch == 'conv-unet':
-                    B, C = x_t.shape[:2]
-                elif self.model_arch == '1d-unet':
-                    B, C = x_t.size(0), x_t.size(1)
-                else:
-                    B, C = x_t.size(0), x_t.size(-1)
-                # B, C = x_t.shape[:2]
-
-                if self.model_arch == 'conv-unet':
-                    assert model_output.shape == (B, C * 2, *x_t.shape[2:])
-                    model_output, model_var_values = th.split(model_output, C, dim=1)
-                    frozen_out = th.cat([model_output.detach(), model_var_values], dim=1)
-                    # print('conv-unet')
-                elif self.model_arch == '1d-unet':
-                    # print(model_output.shape, (B, C * 2, *x_t.shape[2:]))
-                    assert model_output.shape == (B, C * 2, *x_t.shape[2:])
-                    model_output, model_var_values = th.split(model_output, C, dim=1)
-                    frozen_out = th.cat([model_output.detach(), model_var_values], dim=1)
-                else:
-                    # print(model_output.shape, (B, x.size(1), C * 2), x.shape, 'gaussian diffusion.')
-                    assert model_output.shape == (B, x_t.size(1), C * 2)
-                    model_output, model_var_values = th.split(model_output, C, dim=-1)
-                    frozen_out = th.cat([model_output.detach(), model_var_values], dim=-1)
-
-                # assert model_output.shape == (B, C * 2, *x_t.shape[2:])
-                # model_output, model_var_values = th.split(model_output, C, dim=1)
-                # Learn the variance using the variational bound, but don't let
-                # it affect our mean prediction.
-
-
-                terms["vb"] = self._vb_terms_bpd(
-                    model=lambda *args, r=frozen_out: r,
-                    x_start=x_start,
-                    x_t=x_t,
-                    t=t,
-                    clip_denoised=False,
-                    noise=noise,
-                )["output"]
-                if self.loss_type == LossType.RESCALED_MSE:
-                    # Divide by 1000 for equivalence with initial implementation.
-                    # Without a factor of 1/1000, the VB term hurts the MSE term.
-                    terms["vb"] *= self.num_timesteps / 1000.0
-
-            target = {
-                ModelMeanType.PREVIOUS_X: self.q_posterior_mean_variance(
-                    x_start=x_start, x_t=x_t, t=t
-                )[0],
-                ModelMeanType.START_X: x_start,
-                ModelMeanType.EPSILON: noise,
-            }[self.model_mean_type]
-            assert model_output.shape == target.shape == x_start.shape
-            terms["mse"] = mean_flat((target - model_output) ** 2)
-            if "vb" in terms:
-                terms["loss"] = terms["mse"] + terms["vb"]
-            else:
-                terms["loss"] = terms["mse"]
-        else:
-            raise NotImplementedError(self.loss_type)
-
-        return terms
-
     def get_x_start(self, x_start_mean, std):
         '''
         Using the interpolating policy OR using the convolution policy...
@@ -1319,8 +1216,7 @@ class GaussianDiffusion:
             raise NotImplementedError(self.model_mean_type)
         return {'pred_xprev':pred_prev, 'pred_xstart':pred_xstart}
 
-
-    def training_losses_e2e(self, model, x_start, t, model_kwargs=None, noise=None):
+    def training_losses_actual(self, model, x_start, t, model_kwargs=None, noise=None):
         """
         Compute training losses for a single timestep.
 
@@ -1342,8 +1238,7 @@ class GaussianDiffusion:
         std = _extract_into_tensor(self.sqrt_one_minus_alphas_cumprod,
                                    th.tensor([0]).to(x_start_mean.device),
                                    x_start_mean.shape)
-        # print(std.shape, )
-        x_start_log_var = 2 * th.log(std)
+
         x_start = self.get_x_start(x_start_mean, std)
         # print(x_start_mean.shape, x_start.shape)
         if noise is None:
@@ -1353,206 +1248,40 @@ class GaussianDiffusion:
 
         terms = {}
 
-        if self.loss_type == LossType.E2E_KL:
-            terms["loss"] = self._vb_terms_bpd_e2e(
-                model=model,
-                x_start=x_start,
-                x_t=x_t,
-                t=t,
-                input_ids=input_ids,
-                get_logits=get_logits,
-                x_start_mean=x_start_mean, x_start_log_var=x_start_log_var,
-                clip_denoised=False,
-                model_kwargs=model_kwargs,
-                noise=noise,
-            )["output"]
-            # if self.loss_type == LossType.RESCALED_KL:
-            terms["loss"] *= self.num_timesteps
 
-
-        elif self.loss_type == LossType.E2E_MSE or self.loss_type == LossType.E2E_RESCALED_MSE:
-            # print(x_t.shape)
-            model_output = model(x_t, self._scale_timesteps(t), **model_kwargs)
-
-            if self.model_var_type in [
-                ModelVarType.LEARNED,
-                ModelVarType.LEARNED_RANGE,
-            ]:
-                B, C = x_t.size(0), x_t.size(-1)
-                if self.model_arch == 'conv-unet':
-                    assert model_output.shape == (B, C * 2, *x_t.shape[2:])
-                    model_output, model_var_values = th.split(model_output, C, dim=1)
-                    frozen_out = th.cat([model_output.detach(), model_var_values], dim=1)
-                    # print('conv-unet')
-                else:
-                    # print(model_output.shape, (B, x.size(1), C * 2), x.shape, 'gaussian diffusion.')
-                    assert model_output.shape == (B, x_t.size(1), C * 2)
-                    model_output, model_var_values = th.split(model_output, C, dim=-1)
-                    frozen_out = th.cat([model_output.detach(), model_var_values], dim=-1)
-                terms["vb"] = self._vb_terms_bpd_e2e(
-                    model=lambda *args, r=frozen_out: r,
-                    x_start=x_start,
-                    x_t=x_t,
-                    t=t,
-                    input_ids=input_ids,
-                    get_logits=get_logits,
-                    x_start_mean=x_start_mean, x_start_log_var=x_start_log_var,
-                    clip_denoised=False,
-                    noise=noise,
-                )["output"]
-                if self.loss_type == LossType.E2E_RESCALED_MSE:
-                    # Divide by 1000 for equivalence with initial implementation.
-                    # Without a factor of 1/1000, the VB term hurts the MSE term.
-                    terms["vb"] *= self.num_timesteps / 1000.0
-
-            target = {
-                ModelMeanType.PREVIOUS_X: self.q_posterior_mean_variance(
-                    x_start=x_start, x_t=x_t, t=t
-                )[0],
-                ModelMeanType.START_X: x_start,
-                ModelMeanType.EPSILON: noise,
-            }[self.model_mean_type]
-            assert model_output.shape == target.shape == x_start.shape
-            terms["mse"] = mean_flat((target - model_output) ** 2)
-            # print( terms["mse"])
-            model_out_x_start = self.x0_helper(model_output, x_t, t)['pred_xstart']
-            t0_mask = (t == 0)
-            t0_loss = mean_flat((x_start_mean - model_out_x_start) ** 2)
-            # print(terms["mse"].shape, )
-            terms["mse"] = th.where(t0_mask, t0_loss, terms["mse"])
-
-            # tT_mask = (t == self.num_timesteps - 1)
-            out_mean, _, _ = self.q_mean_variance(x_start, th.LongTensor([self.num_timesteps - 1]).to(x_start.device))
-            tT_loss =  mean_flat(out_mean ** 2)
-
-            decoder_nll = self.token_discrete_loss(x_start, get_logits, input_ids)
-
-            # assert (model.lm_head.weight == model.word_embedding.weight).all()
-
-            if "vb" in terms:
-                terms["loss"] = terms["mse"] + terms["vb"]
-            else:
-                # KEY
-                terms["loss"] = terms["mse"] + (decoder_nll + tT_loss)
-                # terms["loss"] = terms["mse"] + (1.0/self.num_timesteps) * decoder_nll + \
-                #                 (1.0/self.num_timesteps) * tT_loss
-        else:
-            raise NotImplementedError(self.loss_type)
-
-        return terms
-
-    def training_losses_e2e_simple(self, model, x_start, t, model_kwargs=None, noise=None):
-        """
-        Compute training losses for a single timestep.
-
-        :param model: the model to evaluate loss on.
-        :param x_start: the [N x C x ...] tensor of inputs.
-        :param t: a batch of timestep indices.
-        :param model_kwargs: if not None, a dict of extra keyword arguments to
-            pass to the model. This can be used for conditioning.
-        :param noise: if specified, the specific Gaussian noise to try to remove.
-        :return: a dict with the key "loss" containing a tensor of shape [N].
-                 Some mean or variance settings may also have other keys.
-        """
-        assert 'input_ids' in model_kwargs
-        x_start = None
-        input_ids = model_kwargs.pop('input_ids').to(t.device)
-        x_start_mean = model.model.module.get_embeds(input_ids)
-        if self.model_arch == 'conv-unet':
-            seqlen = int(np.sqrt(input_ids.size(1)))
-            x_start_mean = x_start_mean.view(x_start_mean.size(0), seqlen, seqlen, x_start_mean.size(-1)).permute(0, 3,
-                                                                                                                  1, 2)
-        elif self.model_arch == '1d-unet':
-            x_start_mean = x_start_mean.permute(0, 2, 1)
-        x_start = x_start_mean
-        # print(x_start_mean.shape, x_start.shape)
-        if noise is None:
-            noise = th.randn_like(x_start)
-        x_t = self.q_sample(x_start, t, noise=noise) # reparametrization trick.
-        get_logits = model.model.module.get_logits
-
-        terms = {}
-
-        if self.loss_type == LossType.E2E_Simple_KL:
-            raise NotImplementedError
+        model_output = model(x_t, self._scale_timesteps(t), **model_kwargs)
 
 
 
-        elif self.loss_type == LossType.E2E_Simple_MSE:
-            # print('simple mse training ')
-            # print(x_t.shape)
-            model_output = model(x_t, self._scale_timesteps(t), **model_kwargs)
+        target = {
+            ModelMeanType.PREVIOUS_X: self.q_posterior_mean_variance(
+                x_start=x_start, x_t=x_t, t=t
+            )[0],
+            ModelMeanType.START_X: x_start,
+            ModelMeanType.EPSILON: noise,
+        }[self.model_mean_type]
+        assert model_output.shape == target.shape == x_start.shape
 
-            if self.model_var_type in [
-                ModelVarType.LEARNED,
-                ModelVarType.LEARNED_RANGE,
-            ]:
-                if self.model_arch == 'conv-unet' or self.model_arch == '1d-unet':
-                    B, C = x_t.shape[:2]
-                else:
-                    B, C = x_t.size(0), x_t.size(-1)
+        # the usual diffusion loss
+        terms["mse"] = mean_flat((target - model_output) ** 2)
+        # print( terms["mse"])
+        model_out_x_start = self.x0_helper(model_output, x_t, t)['pred_xstart']
+        t0_mask = (t == 0)
 
-                if self.model_arch == 'conv-unet':
-                    assert model_output.shape == (B, C * 2, *x_t.shape[2:])
-                    model_output, model_var_values = th.split(model_output, C, dim=1)
-                    frozen_out = th.cat([model_output.detach(), model_var_values], dim=1)
-                    # print('conv-unet')
-                else:
-                    # print(model_output.shape, (B, x.size(1), C * 2), x.shape, 'gaussian diffusion.')
-                    assert model_output.shape == (B, x_t.size(1), C * 2)
-                    model_output, model_var_values = th.split(model_output, C, dim=-1)
-                    frozen_out = th.cat([model_output.detach(), model_var_values], dim=-1)
+        # The embedding loss!
+        t0_loss = mean_flat((x_start_mean - model_out_x_start) ** 2)
+        # print(terms["mse"].shape, )
+        terms["mse"] = th.where(t0_mask, t0_loss, terms["mse"])
+
+        # tT_mask = (t == self.num_timesteps - 1)
+        out_mean, _, _ = self.q_mean_variance(x_start, th.LongTensor([self.num_timesteps - 1]).to(x_start.device))
+        tT_loss =  mean_flat(out_mean ** 2)
+
+        # The rounding loss
+        decoder_nll = self.token_discrete_loss(x_start, get_logits, input_ids)
 
 
-                terms["vb"] = self._vb_terms_bpd_e2e(
-                    model=lambda *args, r=frozen_out: r,
-                    x_start=x_start,
-                    x_t=x_t,
-                    t=t,
-                    input_ids=input_ids,
-                    get_logits=get_logits,
-                    x_start_mean=x_start_mean, x_start_log_var=x_start_log_var,
-                    clip_denoised=False,
-                    noise=noise,
-                )["output"]
-                if self.loss_type == LossType.E2E_RESCALED_MSE:
-                    # Divide by 1000 for equivalence with initial implementation.
-                    # Without a factor of 1/1000, the VB term hurts the MSE term.
-                    terms["vb"] *= self.num_timesteps / 1000.0
-
-            target = {
-                ModelMeanType.PREVIOUS_X: self.q_posterior_mean_variance(
-                    x_start=x_start, x_t=x_t, t=t
-                )[0],
-                ModelMeanType.START_X: x_start,
-                ModelMeanType.EPSILON: noise,
-            }[self.model_mean_type]
-            assert model_output.shape == target.shape == x_start.shape
-            # terms["mse"] = mean_flat((target - model_output) ** 2)
-
-            ce = th.nn.CrossEntropyLoss(reduction='none')
-            model_logits = get_logits(model_output)
-            # print(model_logits.shape)
-            ce_loss = ce(model_logits.view(-1, model_logits.size(-1)), input_ids.view(-1))
-            ce_loss = ce_loss.view(input_ids.shape)
-            # print(ce_loss.shape)
-            terms["ce"] = mean_flat(ce_loss)
-            # print(terms["ce"].shape)
-
-            # out_mean, _, _ = self.q_mean_variance(x_start, th.LongTensor([self.num_timesteps - 1]).to(x_start.device))
-            # tT_loss =  mean_flat(out_mean ** 2)
-
-            # decoder_nll = self.token_discrete_loss(x_start, get_logits, input_ids) / model_output.size(-1) #DEBUG, only true for transformer model.
-
-            if "vb" in terms:
-                terms["loss"] = terms["mse"] + terms["vb"]
-            else:
-                # KEY
-                # terms["loss"] = terms["mse"] + (decoder_nll + tT_loss)
-                terms["loss"] = terms["ce"] # + tT_loss
-
-        else:
-            raise NotImplementedError(self.loss_type)
+        terms["loss"] = terms["mse"] + (decoder_nll + tT_loss)
 
         return terms
 
