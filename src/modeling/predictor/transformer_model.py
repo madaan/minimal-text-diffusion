e462ce944bfe4b96934cc02d5f6993eb981df622
Aman Madaan <amadaan@cs.cmu.edu>
2022-09-25 15:10:13 -0400

    [refactor] remove copied references to UNET, extraneous args

--- src/modeling/predictor/transformer_model.py
+++ src/modeling/predictor/transformer_model.py
@@ -4,8 +4,6 @@ from transformers import AutoConfig
 from transformers.models.bert.modeling_bert import BertEncoder
 import torch
 
-
-import numpy as np
 import torch as th
 import torch.nn as nn
 from src.modeling.diffusion.nn import (
@@ -17,20 +15,13 @@ from src.modeling.diffusion.nn import (
 
 class TransformerNetModel(nn.Module):
     """
-    The full UNet model with attention and timestep embedding.
+    A transformer model to be used in Diffusion Model Training.
 
     :param in_channels: channels in the input Tensor.
     :param model_channels: base channel count for the model.
     :param out_channels: channels in the output Tensor.
-    :param num_res_blocks: number of residual blocks per downsample.
-    :param attention_resolutions: a collection of downsample rates at which
-        attention will take place. May be a set, list, or tuple.
-        For example, if this contains 4, then at 4x downsampling, attention
-        will be used.
     :param dropout: the dropout probability.
     :param channel_mult: channel multiplier for each level of the UNet.
-    :param conv_resample: if True, use learned convolutions for upsampling and
-        downsampling.
     :param dims: determines if the signal is 1D, 2D, or 3D.
     :param num_classes: if specified (as an int), then this model will be
         class-conditional with `num_classes` classes.
@@ -43,29 +34,16 @@ class TransformerNetModel(nn.Module):
         in_channels,
         model_channels,
         out_channels,
-        num_res_blocks,
-        attention_resolutions,
         dropout=0,
-        channel_mult=(1, 2, 4, 8),
-        conv_resample=True,
-        dims=2,
-        num_classes=None,
         use_checkpoint=False,
         num_heads=1,
-        num_heads_upsample=-1,
-        use_scale_shift_norm=False,
         config=None,
         config_name="bert-base-uncased",
-        training_mode="emb",
         vocab_size=None,
-        experiment_mode="lm",
         init_pretrained=False,
         logits_mode=1,
     ):
         super().__init__()
-
-        if num_heads_upsample == -1:
-            num_heads_upsample = num_heads
 
         if config is None:
             config = AutoConfig.from_pretrained(config_name)
@@ -75,15 +53,9 @@ class TransformerNetModel(nn.Module):
         self.in_channels = in_channels
         self.model_channels = model_channels
         self.out_channels = out_channels
-        self.num_res_blocks = num_res_blocks
-        self.attention_resolutions = attention_resolutions
         self.dropout = dropout
-        self.channel_mult = channel_mult
-        self.conv_resample = conv_resample
-        self.num_classes = num_classes
         self.use_checkpoint = use_checkpoint
         self.num_heads = num_heads
-        self.num_heads_upsample = num_heads_upsample
         self.logits_mode = logits_mode
 
         self.word_embedding = nn.Embedding(vocab_size, self.in_channels)
@@ -104,10 +76,6 @@ class TransformerNetModel(nn.Module):
             linear(time_embed_dim, config.hidden_size),
         )
 
-        if self.num_classes is not None:
-            self.label_emb = nn.Embedding(num_classes, time_embed_dim)
-
-        # self.input_up_proj = trans_nd(config, in_channels, model_channels // attention_head_size, attention_head_size)
         self.input_up_proj = nn.Sequential(
             nn.Linear(in_channels, config.hidden_size),
             nn.Tanh(),
@@ -131,13 +99,9 @@ class TransformerNetModel(nn.Module):
         self.position_embeddings = nn.Embedding(
             config.max_position_embeddings, config.hidden_size
         )
-        # self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-
-        # config2 = config
-        # config2.hidden_size = 2 * config.hidden_size
-        # self.output_transformers = BertEncoder(config)
 
         self.output_down_proj = nn.Sequential(
             nn.Linear(config.hidden_size, config.hidden_size),
@@ -149,27 +113,7 @@ class TransformerNetModel(nn.Module):
         return self.word_embedding(input_ids)
 
     def get_logits(self, hidden_repr):
-        if self.logits_mode == 1:
-            return self.lm_head(hidden_repr)
-        elif self.logits_mode == 2:
-            text_emb = hidden_repr
-            emb_norm = (self.lm_head.weight**2).sum(-1).view(-1, 1)  # vocab
-            text_emb_t = th.transpose(
-                text_emb.view(-1, text_emb.size(-1)), 0, 1
-            )  # d, bsz*seqlen
-            arr_norm = (text_emb**2).sum(-1).view(-1, 1)  # bsz*seqlen, 1
-            dist = (
-                emb_norm
-                + arr_norm.transpose(0, 1)
-                - 2.0 * th.mm(self.lm_head.weight, text_emb_t)
-            )  # (vocab, d) x (d, bsz*seqlen)
-            scores = th.sqrt(th.clamp(dist, 0.0, np.inf)).view(
-                emb_norm.size(0), hidden_repr.size(0), hidden_repr.size(1)
-            )  # vocab, bsz*seqlen
-            scores = -scores.permute(1, 2, 0).contiguous()
-            return scores
-        else:
-            raise NotImplementedError
+        return self.lm_head(hidden_repr)
 
     def forward(self, x, timesteps, y=None, src_ids=None, src_mask=None):
         """
@@ -204,35 +148,3 @@ class TransformerNetModel(nn.Module):
         h = self.output_down_proj(input_trans_hidden_states)
         h = h.type(x.dtype)
         return h
-
-    def get_feature_vectors(self, x, timesteps, y=None):
-        """
-        Apply the model and return all of the intermediate tensors.
-
-        :param x: an [N x C x ...] Tensor of inputs.
-        :param timesteps: a 1-D batch of timesteps.
-        :param y: an [N] Tensor of labels, if class-conditional.
-        :return: a dict with the following keys:
-                 - 'down': a list of hidden state tensors from downsampling.
-                 - 'middle': the tensor of the output of the lowest-resolution
-                             block in the model.
-                 - 'up': a list of hidden state tensors from upsampling.
-        """
-        hs = []
-        emb = self.time_embed(timestep_embedding(timesteps, self.model_channels))
-        if self.num_classes is not None:
-            assert y.shape == (x.shape[0],)
-            emb = emb + self.label_emb(y)
-        result = dict(down=[], up=[])
-        h = x.type(self.inner_dtype)
-        for module in self.input_blocks:
-            h = module(h, emb)
-            hs.append(h)
-            result["down"].append(h.type(x.dtype))
-        h = self.middle_block(h, emb)
-        result["middle"] = h.type(x.dtype)
-        for module in self.output_blocks:
-            cat_in = th.cat([h, hs.pop()], dim=-1)
-            h = module(cat_in, emb)
-            result["up"].append(h.type(x.dtype))
-        return result
